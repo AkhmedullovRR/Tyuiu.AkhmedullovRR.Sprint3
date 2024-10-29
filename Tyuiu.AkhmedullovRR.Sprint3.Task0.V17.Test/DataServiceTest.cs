ca15b82975644d6afedf1be8ee55ef51cc11b3d5
Равиль <fullbotrr@mail.ru>
2024-10-30 01:25:32 +0300

    Add project files.

---
+++ Tyuiu.AkhmedullovRR.Sprint3.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+using Tyuiu.AkhmedullovRR.Sprint3.Task0.V17.Lib;
+
+namespace Tyuiu.AkhmedullovRR.Sprint3.Task0.V17.Test
+
+
+{
+   
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int start = 1; int end = 10;
+            var res = ds.GetSumSeries(start, end);
+            Assert.AreEqual(-0.709, res);
+        }
+    }
+}
---
+++ Tyuiu.AkhmedullovRR.Sprint3.Task0.V17/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AkhmedullovRR.Sprint3.Task0.V17.Lib;
+
+namespace Tyuiu.AkhmedullovRR.Sprint3.Task0.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Ахмедуллов Р.Р | ИСТНБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема:  Оператор цикла for                                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Ахмедуллов Равиль Радикович | ИСТНБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда по  *");
+            Console.WriteLine("* формуле s = E cos(i) * 1/2  ( конечное слагаемое = 10, начальное = i )  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("i = 1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            double res = ds.GetSumSeries(1, 10);
+            Console.WriteLine(res);
+        }
+    }
+}
