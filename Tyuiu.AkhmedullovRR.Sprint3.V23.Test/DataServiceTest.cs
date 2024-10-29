7944f5c8a421bfb27d810577e69aa26fc78dfa79
Равиль <fullbotrr@mail.ru>
2024-10-30 02:41:28 +0300

    5464

---
+++ Tyuiu.AkhmedullovRR.Sprint3.V23.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.AkhmedullovRR.Sprint3.V23.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
