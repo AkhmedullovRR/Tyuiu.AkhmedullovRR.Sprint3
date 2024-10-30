02c8da7ce2a5cd3818dd44bfffed7723e806bfd7
Равиль <fullbotrr@mail.ru>
2024-10-30 03:32:14 +0300

    ref

---
+++ Tyuiu.AkhmedullovRR.Sprint3.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.AkhmedullovRR.Sprint3.Task3.V4.Lib;
+
+namespace Tyuiu.AkhmedullovRR.Sprint3.Task3.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string str = "plkjjdw cvjkl";
+            char c = 'j';
+            string wait = "plk  dw cv kl";
+            string res = ds.DeleteCharInString(str, c);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AkhmedullovRR.Sprint3.Task3.V4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AkhmedullovRR.Sprint3.Task3.V4.Lib;
+
+namespace Tyuiu.AkhmedullovRR.Sprint3.Task3.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string str = "plkjjdw cvjkl";
+            char c = 'j';
+
+            Console.Title = "Спринт #3 | Выполнил: Ахмедуллов Р.Р | ИТСНБ-24-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Ахмедуллов Равиль Радикович | ИСТНБ-24-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл foreach, которая удаляет из строки    *");
+            Console.WriteLine("* все буквы j                                                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Строка =  " + str);
+            Console.WriteLine("Удаляемый символ = " + c);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Конечная строка : " + ds.DeleteCharInString(str, c));
+            Console.ReadKey();
+        }
+    }
+}
